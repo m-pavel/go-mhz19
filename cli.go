6598bf2fa55b9fef1b540aabf45cbba94a21325c
pavel m <paullus325@gmail.com>
2019-09-26 09:39:01 +0300

    sync

--- cli.go
+++ cli.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 
+	"flag"
+
 	"github.com/m-pavel/go-mhz19/pkg"
 )
 
 func main() {
-	mhz := mhz19.NewSerial()
+	device := flag.String("device", "/dev/serial0", "Serial device")
+	flag.Parse()
+	mhz := mhz19.NewSerial(*device)
 	if err := mhz.Open(); err != nil {
 		panic(err)
 	}
