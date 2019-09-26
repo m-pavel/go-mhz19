f5d6ceecb98bb50ad4e1fffc3401b6735984bc19
pavel m <paullus325@gmail.com>
2019-09-26 09:37:52 +0300

    sync

--- mqtt/mqtt.go
+++ mqtt/mqtt.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"flag"
+
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 	"github.com/m-pavel/go-hassio-mqtt/pkg"
 	"github.com/m-pavel/go-mhz19/pkg"
 )
 
 type Co2Service struct {
-	m mhz19.Mhz19
+	m      mhz19.Mhz19
+	device *string
 }
 
-func (ts Co2Service) PrepareCommandLineParams() {}
-func (ts Co2Service) Name() string              { return "mhz19" }
+func (ts *Co2Service) PrepareCommandLineParams() {
+	ts.device = flag.String("device", "/dev/serial0", "Serial device")
+}
+func (ts Co2Service) Name() string { return "mhz19" }
 
 func (ts *Co2Service) Init(client MQTT.Client, topic, topicc, topica string, debug bool, ss ghm.SendState) error {
-	ts.m = mhz19.NewSerial()
+	ts.m = mhz19.NewSerial(*ts.device)
 	return ts.m.Open()
 }
 
