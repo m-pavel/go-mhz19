4c383ed822acdb2c9c9d0f5fcf23292684d52159
pavel m <paullus325@gmail.com>
2023-04-25 11:44:58 +0200

    axis

--- http/cli.go
+++ http/cli.go
@@ -7,9 +7,10 @@ import (
 )
 
 func Converter(r *co2.Readings) ghm.Entry {
-	return ghm.Entry{"co2": r.Co2, "temp": r.Temperature}
+	//return ghm.Entry{"co2": r.Co2, "temp": r.Temperature}
+	return ghm.Entry{"value": r.Co2}
 }
 
 func main() {
-	ghm.NewExecutor[*co2.Readings]("co2", &producer.Co2Service{}, &ghm.HttpServer[*co2.Readings]{ToRawConverter: Converter, YAxis: []string{"co2", "temp"}}).Main()
+	ghm.NewExecutor[*co2.Readings]("co2", &producer.Co2Service{}, &ghm.HttpServer[*co2.Readings]{ToRawConverter: Converter}).Main()
 }
